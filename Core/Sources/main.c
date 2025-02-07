dc1daaaa414bebd1b287b3357fef82887bb676c4
benjami-r <benjami_r@yahoo.com>
2025-02-07 11:35:59 +0200

    blink

--- Core/Sources/main.c
+++ Core/Sources/main.c
@@ -1,8 +1,11 @@
-//#include "stm32f072xb.h" //включит "core_cm0.h"
+#include "stm32f072xb.h" //включит "core_cm0.h"
 
-unsigned int q =0x12345678;
-unsigned int w;
 int main() {
+	*((volatile uint32_t*)RCC_BASE + 0x14) 		|= RCC_AHBENR_GPIOAEN;
+	*((volatile uint32_t*)GPIOA_BASE + 0x00)	&= ~GPIO_MODER_MODER5;
+	*((volatile uint32_t*)GPIOA_BASE + 0x00)	|= GPIO_MODER_MODER5_0;
+	*((volatile uint32_t*)GPIOA_BASE + 0x18)	|= GPIO_BSRR_BS_5;
+
 
 	return 0;
 }
