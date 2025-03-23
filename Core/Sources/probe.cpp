7d5b1cf52e5879cc1cca0e7fe8e27111526d0203
benjami_r <benjami_r@yahoo.com>
2025-03-23 09:06:10 +0200

    next

--- Core/Sources/probe.cpp
+++ Core/Sources/probe.cpp
@@ -1,3 +1,6 @@
+#include <stdint.h>
+
+
 class C {
 	int a = 5;
 public:
@@ -11,3 +14,47 @@ extern "C" {
 		return c->aGet();
 	}
 }
+
+template<uint32_t Addr>
+class Register {
+public:
+    static void write(uint32_t value) {
+        *reinterpret_cast<volatile uint32_t*>(Addr) = value;
+    }
+
+    static uint32_t read() {
+        return *reinterpret_cast<volatile uint32_t*>(Addr);
+    }
+};
+
+// Использование:
+using GPIOA_MODER = Register<0x48000000>;  // Адрес регистра MODER для GPIOA
+
+void init() {
+	GPIOA_MODER::write(0x55555555);  // Настройка GPIOA
+}
+
+
+//---------------------------------------------------
+template <typename Derived>
+class Handler {
+public:
+    void handle() {
+        static_cast<Derived*>(this)->onEvent();
+    }
+};
+
+class ButtonHandler : public Handler<ButtonHandler> {
+public:
+    void onEvent() {
+        // Реакция на нажатие кнопки
+    }
+};
+void init1() {
+	// Использование:
+	ButtonHandler btn;
+	btn.handle();  // Компилятор подставит прямой вызов onEvent()
+}
+
+
+
---
+++ a_gcc-arm-none-eabi.cmake
@@ -0,0 +1,23 @@
+# gcc-arm-none-eabi.cmake
+
+#TODO:
+# c60_NEW_gcc-arm-none-eabi.cmake
+
+###################### TOOLCHAIN ######################################
+set(CMAKE_SYSTEM_NAME              "Generic")      #My: mandatory (требуется обязательно, действует принудительно)
+set(CMAKE_SYSTEM_PROCESSOR         "arm")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE  STATIC_LIBRARY) #My: Skip link step during toolchain validation
+
+set(TOOLCHAIN_PREFIX               "arm-none-eabi-")
+set(CMAKE_C_COMPILER               "${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_ASM_COMPILER             "${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_CXX_COMPILER             "${TOOLCHAIN_PREFIX}g++")
+#set(CMAKE_LINKER                   "${TOOLCHAIN_PREFIX}ld")
+set(CMAKE_LINKER                   "${TOOLCHAIN_PREFIX}g++") #MX
+set(CMAKE_OBJCOPY                  "${TOOLCHAIN_PREFIX}objcopy")
+set(CMAKE_SIZE                     "${TOOLCHAIN_PREFIX}size")
+#set(CMAKE_AR                       "${TOOLCHAIN_PREFIX}ar")
+#set(CMAKE_RANLIB                   "${TOOLCHAIN_PREFIX}ranlib")
+#set(CMAKE_STRIP                    "${TOOLCHAIN_PREFIX}ld")
+
